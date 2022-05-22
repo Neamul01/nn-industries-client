39d2b65e23d8059815ea5d61d5644eff14aad59e
Neamul01 <neamulhaque2002@gmail.com>
2022-05-23 00:33:22 +0600

    framework and component library added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
 import { Routes } from 'react-router-dom';
+import Navbar from './components/Shared/Navbar';
 
 function App() {
   return (
-    <div className="App">
+    <div className="mx-4 md:mx-8 lg:mx-12">
+      <Navbar></Navbar>
       <Routes>
 
       </Routes>
