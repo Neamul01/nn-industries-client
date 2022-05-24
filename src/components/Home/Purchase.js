6f7c28327de2c15e89321e3092dffe823f3da2a3
Neamul01 <neamulhaque2002@gmail.com>
2022-05-24 14:49:02 +0600

    single product loaded at purchase page

--- src/components/Home/Purchase.js
+++ src/components/Home/Purchase.js
@@ -1,31 +1,58 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { useAuthState } from 'react-firebase-hooks/auth'
+import { useQuery } from 'react-query';
 import { useParams } from 'react-router-dom';
+import { toast } from 'react-toastify';
 import auth from '../../firebase.init';
+import LoadingSpinner from '../Shared/LoadingSpinner';
+import { request } from '../utils/axios-utils';
 
 
 const Purchase = () => {
     const [user] = useAuthState(auth);
     const { id } = useParams();
-    console.log(id)
-    return (
+    const [btnDisable, setBtnDisable] = useState(false)
 
+    const { data: singleProduct, isLoading, error } = useQuery('singleProduct', async () => await request({ url: `/products/${id}`, method: 'get' }))
+
+    // console.log(Number(quantity))
+    if (isLoading) {
+        return <LoadingSpinner></LoadingSpinner>
+    }
+    if (error) {
+        toast.error(error.message)
+    }
+    const { product, price, minimumQuantity, image, description, availableQuantity } = singleProduct;
+
+    const handleQuantity = inputQuantity => {
+        // console.log(inputQuantity)
+        // if (inputQuantity < minimumQuantity || inputQuantity > availableQuantity) {
+        //     console.log('big')
+        //     setBtnDisable(true)
+        // }
+        if (inputQuantity > minimumQuantity || inputQuantity < availableQuantity) {
+            setBtnDisable(false)
+            console.log('min')
+        }
+        console.log('low')
+    }
+
+    return (
         <section className=" dark:bg-gray-900 lg:py-12 lg:flex lg:justify-around">
             <div className="dark:bg-gray-800 lg:mx-8 lg:flex lg:max-w-5xl lg:shadow-lg lg:rounded-lg w-full justify-around">
                 <div className="lg:w-1/2">
-                    <div className="h-64 bg-cover lg:rounded-lg lg:h-full" style={{ backgroundImage: "url('https://images.unsplash.com/photo-1593642532400-2682810df593?ixlib=rb-1.2.1&ixid=MXwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=750&q=80')" }}></div>
+                    <div className="h-64 bg-cover lg:rounded-lg lg:h-full" style={{ backgroundImage: `url(${image})` }}></div>
                 </div>
 
                 <div className="max-w-xl px-6 py-12 lg:max-w-5xl lg:w-1/2">
                     <div className="card-body items-center text-center">
-                        <h2 className="card-title">Shoes!</h2>
-                        <p>{id}</p>
-                        <p>If a dog chews shoes whose shoes does he choose?</p>
+                        <h2 className="card-title">{product}</h2>
+                        <p className='text-left border-y-2 py-2 my-3'>{description}</p>
                         <div className="flex w-full">
-                            <p className='text-left font-bold'>Min Quantity: </p>
-                            <p className='text-left font-bold'>Available:</p>
+                            <p className='text-left font-bold'>Min Quantity: {minimumQuantity} </p>
+                            <p className='text-left font-bold'>Available: {availableQuantity} </p>
                         </div>
-                        <p className="text-left font-bold w-full ">Price:</p>
+                        <p className="text-left font-bold w-full ">Price:{price} </p>
                         <div className='w-full my-4'>
                             <div >
                                 <label htmlFor="username" className="block text-sm text-gray-800 dark:text-gray-200 text-left">Name</label>
@@ -53,11 +80,14 @@ const Purchase = () => {
                             <div className='w-full'>
                                 <label htmlFor="username" className="block text-sm text-gray-800 dark:text-gray-200 text-left">Quantity</label>
                                 <input type="number"
+                                    onBlur={e => handleQuantity(e.target.value)}
                                     className="block w-1/2 px-4 py-2 mt-2 text-gray-700 bg-white border rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 dark:focus:border-blue-300 focus:ring-blue-300 focus:outline-none focus:ring focus:ring-opacity-40" />
                                 <p className="text-xs text-gray-600 dark:text-gray-400 text-left">"Quantity should be higher then minimum quantity and less then total quantity."</p>
                             </div>
                         </div>
-                        <button className="btn w-full btn-primary text-left">Place Order</button>
+                        <button
+                            disabled={btnDisable}
+                            className="btn w-full btn-primary text-left">Place Order</button>
                     </div>
                 </div>
             </div>
