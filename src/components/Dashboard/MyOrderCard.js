d833541dc02ff84bab031ce3c1f98614a09d1b9d
Neamul01 <neamulhaque2002@gmail.com>
2022-05-26 00:32:31 +0600

    my order card component created show data and delete order

---
+++ src/components/Dashboard/MyOrderCard.js
@@ -0,0 +1,46 @@
+import React from 'react';
+import { request } from '../utils/axios-utils';
+
+const MyOrderCard = ({ order, refetch }) => {
+    const { _id, name, address, quantity, price, image } = order;
+    // console.log(order)
+
+    const handleDelete = () => {
+        request({ url: `/orders/${_id}`, method: 'delete' })
+            .then(res => {
+                console.log(res)
+                refetch();
+            })
+            .catch(err => {
+                console.log(err)
+            })
+    }
+
+    return (
+        <div className="flex max-w-4xl w-full mx-auto overflow-hidden rounded-lg shadow-lg dark:bg-gray-800">
+            <div className="w-1/3 bg-cover m-2 rounded-md" style={{ backgroundImage: `url(${image})`, backgroundSize: 'cover', backgroundPosition: 'center' }}></div>
+
+            <div className="w-2/3 p-4 md:p-4 flex justify-between">
+                <div className='flex flex-col gap-2'>
+                    <h1 className="text-2xl font-bold text-gray-800 dark:text-white">{name}</h1>
+                    <p className="mt-2 text-sm text-gray-600 dark:text-gray-400">Address: {address}</p>
+                    <div className='flex gap-2 md:gap-6'>
+                        <h3 className="text-md font-semibold text-gray-700 dark:text-gray-200 md:text-md">Quantity: {quantity}</h3>
+                        <h3 className="text-md font-semibold text-gray-700 dark:text-gray-200 md:text-md">Price: ${price}</h3>
+                    </div>
+                    <h3 className="text-lg font-bold text-gray-700 dark:text-gray-200 md:text-xl">Total price: ${price * quantity}</h3>
+                </div>
+
+                <div className="flex justify-around mt-3 item-center flex-col ">
+                    <button
+                        className="px-2 py-1 text-xs max-h-8 font-bold text-white uppercase transition-colors duration-200 transform bg-accent rounded dark:bg-gray-700 hover:bg-gray-700 dark:hover:bg-gray-600 focus:outline-none focus:bg-gray-700 dark:focus:bg-gray-600">Pay</button>
+                    <button
+                        onClick={handleDelete}
+                        className="px-2 py-1 text-xs  font-bold text-white uppercase transition-colors duration-200 transform bg-red-500 rounded dark:bg-red-700 hover:bg-red-700 dark:hover:bg-red-600 focus:outline-none focus:bg-red-700 dark:focus:bg-red-600">Cancel</button>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default MyOrderCard;
--- src/components/Home/Purchase.js
+++ src/components/Home/Purchase.js
@@ -24,7 +24,7 @@ const Purchase = () => {
     if (error) {
         toast.error(error.message)
     }
-    const { _id,product, price, minimumQuantity, image, description, availableQuantity } = singleProduct;
+    const { product, price, minimumQuantity, image, description, availableQuantity } = singleProduct;
 
     const handleQuantity = inputQuantity => {
         if (Number(inputQuantity) < Number(minimumQuantity) || Number(inputQuantity) > Number(availableQuantity)) {
@@ -38,22 +38,24 @@ const Purchase = () => {
     }
 
     const onSubmit = data => {
-        const order={
-            productId:id,
-            name:user.displayName,
-            email:user.email,
-            address:data.address,
-            quantity:data.quantity,
-            phone:data.phone,
+        const order = {
+            productId: id,
+            name: product,
+            email: user.email,
+            address: data.address,
+            quantity: data.quantity,
+            phone: data.phone,
+            price,
+            image,
         }
         // console.log(order)
-        request({url:'/orders',method:'post',data:order})
-        .then(res=>{
-            toast.success('Order placed successfully...')
-        })
-        .catch(err=>{
-            toast.error(err?.message)
-        })
+        request({ url: '/orders', method: 'post', data: order })
+            .then(res => {
+                toast.success('Order placed successfully...')
+            })
+            .catch(err => {
+                toast.error(err?.message)
+            })
         reset()
     };
 
@@ -74,73 +76,73 @@ const Purchase = () => {
                         </div>
                         <p className="text-left font-bold w-full ">Price:{price} </p>
                         <form onSubmit={handleSubmit(onSubmit)} className="mt-6 w-full">
-                        <div className='w-full'>
-                            <label htmlFor="Name" className="block text-sm text-gray-800 dark:text-gray-200 text-left">Name</label>
-                            <input type="text"
-                            defaultValue={user?.displayName||''}
-                            disabled
-                                className="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 dark:focus:border-blue-300 focus:ring-blue-300 focus:outline-none focus:ring focus:ring-opacity-40" />
-                        </div>
-
-                        <div className='w-full mt-4'>
-                            <label htmlFor="email" className="block text-sm text-gray-800 dark:text-gray-200 text-left">Email</label>
-                            <input type="email"
-                            defaultValue={user?.email||''}
-                            disabled
-                                className="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 dark:focus:border-blue-300 focus:ring-blue-300 focus:outline-none focus:ring focus:ring-opacity-40" />
-                        </div>
-
-                        <div className='w-full mt-4'>
-                            <label htmlFor="address" className="block text-sm text-gray-800 dark:text-gray-200 text-left">Address</label>
-                            
-                            <input type="text"
-                            {...register("address",{
-                                required: true
-                            })}
-                                className="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 dark:focus:border-blue-300 focus:ring-blue-300 focus:outline-none focus:ring focus:ring-opacity-40" />
-                                {errors.address?.type === 'required' && <p className="text-red-500 text-left"><small>"Address is required"</small></p>}
-                        </div>
-
-                        <div className='w-full mt-4'>
-                            <label htmlFor="phone" className="block text-sm text-gray-800 dark:text-gray-200 text-left">Phone Number</label>
-                            
-                            <input type="number"
-                            {...register("phone",{
-                                required: true
-                            })}
-                                className="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 dark:focus:border-blue-300 focus:ring-blue-300 focus:outline-none focus:ring focus:ring-opacity-40" />
-                                {errors.number?.type === 'required' && <p className="text-red-500 text-left"><small>"Number is required"</small></p>}
-                        </div>
-
-                        <div className="mt-4">
-                            <div className="flex items-center justify-between">
-                                <label htmlFor="quantity" className="block text-sm text-gray-800 dark:text-gray-200">Quantity</label>
+                            <div className='w-full'>
+                                <label htmlFor="Name" className="block text-sm text-gray-800 dark:text-gray-200 text-left">Name</label>
+                                <input type="text"
+                                    defaultValue={user?.displayName || ''}
+                                    disabled
+                                    className="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 dark:focus:border-blue-300 focus:ring-blue-300 focus:outline-none focus:ring focus:ring-opacity-40" />
                             </div>
 
-                            <input type="number"
-                            {...register("quantity",{
-                                required: true,
-                                min: minimumQuantity,
-                                max: availableQuantity
-                            })}
-                            onBlur={e=>handleQuantity(e.target.value)}
-                                placeholder='Quantity...'
-                                className="block w-1/3 px-4 py-2 mt-2 text-gray-700 bg-white border rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 dark:focus:border-blue-300 focus:ring-blue-300 focus:outline-none focus:ring focus:ring-opacity-40" />
+                            <div className='w-full mt-4'>
+                                <label htmlFor="email" className="block text-sm text-gray-800 dark:text-gray-200 text-left">Email</label>
+                                <input type="email"
+                                    defaultValue={user?.email || ''}
+                                    disabled
+                                    className="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 dark:focus:border-blue-300 focus:ring-blue-300 focus:outline-none focus:ring focus:ring-opacity-40" />
+                            </div>
+
+                            <div className='w-full mt-4'>
+                                <label htmlFor="address" className="block text-sm text-gray-800 dark:text-gray-200 text-left">Address</label>
+
+                                <input type="text"
+                                    {...register("address", {
+                                        required: true
+                                    })}
+                                    className="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 dark:focus:border-blue-300 focus:ring-blue-300 focus:outline-none focus:ring focus:ring-opacity-40" />
+                                {errors.address?.type === 'required' && <p className="text-red-500 text-left"><small>"Address is required"</small></p>}
+                            </div>
+
+                            <div className='w-full mt-4'>
+                                <label htmlFor="phone" className="block text-sm text-gray-800 dark:text-gray-200 text-left">Phone Number</label>
+
+                                <input type="number"
+                                    {...register("phone", {
+                                        required: true
+                                    })}
+                                    className="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 dark:focus:border-blue-300 focus:ring-blue-300 focus:outline-none focus:ring focus:ring-opacity-40" />
+                                {errors.number?.type === 'required' && <p className="text-red-500 text-left"><small>"Number is required"</small></p>}
+                            </div>
+
+                            <div className="mt-4">
+                                <div className="flex items-center justify-between">
+                                    <label htmlFor="quantity" className="block text-sm text-gray-800 dark:text-gray-200">Quantity</label>
+                                </div>
+
+                                <input type="number"
+                                    {...register("quantity", {
+                                        required: true,
+                                        min: minimumQuantity,
+                                        max: availableQuantity
+                                    })}
+                                    onBlur={e => handleQuantity(e.target.value)}
+                                    placeholder='Quantity...'
+                                    className="block w-1/3 px-4 py-2 mt-2 text-gray-700 bg-white border rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 dark:focus:border-blue-300 focus:ring-blue-300 focus:outline-none focus:ring focus:ring-opacity-40" />
                                 {errors.quantity?.type === 'required' && <p className="text-red-500 text-left"><small>"Quantity is required"</small></p>}
                                 {errors.quantity?.type === 'min' && <p className="text-red-500 text-left"><small>"Quantity should more then minimum Quantity"</small></p>}
                                 {errors.quantity?.type === 'max' && <p className="text-red-500 text-left"><small>"Quantity should lower then Available Quaantity"</small></p>}
                                 {customError && <p className="text-red-500 text-left"><small>{customError}</small></p>}
-                        </div>
+                            </div>
 
-                        <div className="mt-6">
-                            <input type='submit'
-                            value='submit'
-                            disabled={btnDisable===true}
-                                className="w-full px-4 py-2 tracking-wide text-white transition-colors duration-200 transform bg-gray-700 rounded-md hover:bg-gray-600 focus:outline-none focus:bg-gray-600"/>
-                                
-                        </div>
+                            <div className="mt-6">
+                                <input type='submit'
+                                    value='submit'
+                                    disabled={btnDisable === true}
+                                    className="w-full cursor-pointer px-4 py-2 tracking-wide text-white transition-colors duration-200 transform bg-gray-700 rounded-md hover:bg-gray-600 focus:outline-none focus:bg-gray-600" />
 
-                    </form>
+                            </div>
+
+                        </form>
                     </div>
                 </div>
             </div>
